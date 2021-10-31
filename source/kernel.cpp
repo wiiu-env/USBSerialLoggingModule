e34d537040f6beec575cbfe5eb7fe37aaa64fea5
Maschell <Maschell@gmx.de>
2021-10-31 16:35:33 +0100

    Use WUMS 0.2.1

---
+++ source/kernel.cpp
@@ -0,0 +1,92 @@
+#include <cstdint>
+
+#define k_memset ((void (*)( uint32_t , uint32_t, uint32_t))0xfff09d60)
+#define KiReport ((void (*)( const char*, ... ))0xfff0ad0c)
+#define IopShell_AsyncCallback (0xfff1b7d8)
+#define IopShell_ReadCallback (0xfff1b9a0)
+#define k_memcpy ((void (*)(void*,void*, uint32_t))0xfff09e44)
+#define IPCKDriver_OpenAsync ((int32_t (*)(uint32_t client_ram_pid, const char *device, uint32_t mode, uint32_t asyncCallback, void * context))0xfff0b9c8)
+#define WaitAsyncReply ((uint32_t (*)(void* , uint32_t, uint32_t))0xfff180dc)
+
+#define IPCKDriver_IoctlAsync ((int32_t (*)( \
+                                uint32_t clientProcessId,\
+                                uint32_t loaderProcessId,\
+                                uint32_t handle,\
+                                uint32_t request,\
+                                void* inBuf,\
+                                uint32_t inLen,\
+                                void* outBuf,\
+                                uint32_t outLen,\
+                                uint32_t asyncCallback,\
+                                void * context))0xfff0bc5c)
+
+
+uint32_t IPCKDriver_IOS_Open_Sync(uint32_t clientProcessId, const char *device_name, uint32_t mode) {
+    register int r13 asm ("r13");
+    k_memcpy((void *) (r13 - 0x1180), (void *) device_name, 0x10);
+    void *context = (void *) (r13 + 0x450);
+    auto error = IPCKDriver_OpenAsync(clientProcessId, (const char *) (r13 - 0x1180), mode, IopShell_AsyncCallback, context);
+    if (error < 0) {
+        return error;
+    }
+    return WaitAsyncReply(context, 20000000, 6);
+}
+
+uint32_t IPCKDriver_IOS_Ioctl_Sync(uint32_t clientProcessId, uint32_t handle, uint32_t request, void *inBuf, uint32_t inLen, void *outBuf, uint32_t ouLen) {
+    register int r13 asm ("r13");
+    void *context = (void *) (r13 + 0x450);
+
+    auto error = IPCKDriver_IoctlAsync(clientProcessId, 0xFFFFFFFF, handle, request, inBuf, inLen, outBuf, ouLen, IopShell_AsyncCallback, context);
+
+    if (error < 0) {
+        return error;
+    }
+    uint32_t res = WaitAsyncReply(context, 20000000, 6);
+    return res;
+}
+
+
+void IopShellInitInternal() {
+    register int r13 asm ("r13");
+    *((uint32_t *) (r13 + 0x450)) = 0;
+    *((uint32_t *) (r13 + 0x454)) = 0;
+    *((uint32_t *) (r13 + 0x458)) = 0xffffffe3;
+    auto handle = IPCKDriver_IOS_Open_Sync(0, "/dev/iopsh", (uint32_t) 5);
+    if (static_cast<int>(handle) < 0) {
+        KiReport("####################################################################################\n");
+        KiReport("IopShellInit failed\n");
+        KiReport("####################################################################################\n");
+        return;
+    }
+    *(uint32_t *) (r13 + -0x6d84) = static_cast<uint32_t>(handle);
+    *(uint32_t *) (r13 + -0x1178) = 5;
+    *(uint32_t *) (r13 + -0x117c) = 0x1040;
+
+    auto result = IPCKDriver_IOS_Ioctl_Sync(0, static_cast<uint32_t>(handle), 3, (void *) (r13 - 0x1180), 0x14, (void *) (r13 - 0x1140), 0x4);
+    if (static_cast<int>(result) < 0) {
+        KiReport("####################################################################################\n");
+        KiReport("IopShellInit failed\n");
+        KiReport("####################################################################################\n");
+        return;
+    }
+    *(uint32_t *) (r13 + -0x117c) = 0x636F7300; // "cos"
+    result = IPCKDriver_IOS_Ioctl_Sync(0, static_cast<uint32_t>(handle), 1, (void *) (r13 - 0x1180), 0x14, (void *) (r13 - 0x1140), 0x4);
+    if (static_cast<int>(result) < 0) {
+        KiReport("####################################################################################\n");
+        KiReport("IopShellInit failed\n");
+        KiReport("####################################################################################\n");
+        return;
+    }
+    *((uint32_t *) (r13 + 0x450)) = 0;
+    *((uint32_t *) (r13 + 0x454)) = 0;
+    *((uint32_t *) (r13 + 0x458)) = 0xffffffe3;
+    k_memset((r13 - 0x1180), 0, 0x1040);
+    result = IPCKDriver_IoctlAsync(0, 0xFFFFFFFF, handle, 5, nullptr, 0, (void *) (r13 - 0x1180), 0x1040, IopShell_ReadCallback, (void*) (r13 + 0x450));
+    if (static_cast<int>(result) < 0) {
+        KiReport("####################################################################################\n");
+        KiReport("IopShellInit failed\n");
+        KiReport("####################################################################################\n");
+        return;
+    }
+    KiReport("IopShellInit was successful\n");
+}
---
+++ source/main.cpp
@@ -0,0 +1,56 @@
+#include <wums.h>
+#include <coreinit/ios.h>
+#include <coreinit/debug.h>
+#include <coreinit/cache.h>
+#include <kernel/kernel.h>
+#include <whb/log_udp.h>
+#include "kernel.h"
+
+WUMS_MODULE_EXPORT_NAME("homebrew_usbseriallogging");
+WUMS_MODULE_SKIP_ENTRYPOINT();
+WUMS_MODULE_INIT_BEFORE_RELOCATION_DONE_HOOK();
+
+extern "C" void SC_0x51();
+
+WUMS_INITIALIZE(args) {
+    WHBLogUdpInit();
+    auto gModuleData = args.module_information;
+    if (gModuleData == nullptr) {
+        OSFatal("USBSerialLogging: Failed to get gModuleData pointer.");
+    }
+    if (gModuleData->version != MODULE_INFORMATION_VERSION) {
+        OSFatal("USBSerialLogging: The module information struct version does not match.");
+    }
+
+    // Start syslogging on iosu side
+    int mcpFd = IOS_Open("/dev/mcp", (IOSOpenMode) 0);
+    if (mcpFd >= 0) {
+        int in = 0xFA; // IPC_CUSTOM_START_USB_LOGGING
+        int out = 0;
+        IOS_Ioctl(mcpFd, 100, &in, sizeof(in), &out, sizeof(out));
+        IOS_Close(mcpFd);
+    }
+
+    // Patch loader.elf to spit out less warnings when loading .rpx built with wut
+    KernelNOPAtPhysicalAddress(0x0100b770 - 0x01000000 + 0x32000000);
+    KernelNOPAtPhysicalAddress(0x0100b800 - 0x01000000 + 0x32000000);
+    KernelNOPAtPhysicalAddress(0x0100b7b8 - 0x01000000 + 0x32000000);
+    ICInvalidateRange(reinterpret_cast<void *>(0x0100b770), 0x04);
+    ICInvalidateRange(reinterpret_cast<void *>(0x0100b800), 0x04);
+    ICInvalidateRange(reinterpret_cast<void *>(0x0100b7b8), 0x04);
+
+    KernelPatchSyscall(0x51, (uint32_t) &IopShellInitInternal);
+
+    // Start iopshell on kernel
+    SC_0x51();
+}
+
+#define IopShell_UserCallback (0x101C400 + 0x1926c)
+#define IopShell_RegisterCallback ((void (*)( uint32_t,uint32_t,uint32_t,uint32_t))(0x101C400 + 0x19638))
+#define IopShell_CreateThread ((void (*)( void))(0x101C400 + 0x19504))
+
+WUMS_APPLICATION_STARTS(){
+    IopShell_RegisterCallback(IopShell_UserCallback, 0x100978f8, 0x10097900, 0x10097c40);
+    IopShell_CreateThread();
+}
+
